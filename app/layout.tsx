83a759932d89570f8f5c832a5278d66c2233752e
Mateus Berardo de Souza Terra <mateus.b.s.terra@gmail.com>
2023-11-15 17:15:35 -0300

    Add GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,9 @@ import './css/style.css';
 import localFont from 'next/font/local';
 
 import Header from '@/components/ui/header';
+import Script from 'next/script';
 
-const bandung = localFont({src: './BandungRegular.otf', variable: "--font-bandung"})
+const bandung = localFont({ src: './BandungRegular.otf', variable: '--font-bandung' });
 
 export const metadata = {
   title: 'MatTerra - TI-as-a-Service',
@@ -18,6 +19,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4}`}/>
+    <Script strategy="lazyOnload">
+      {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA4}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+    </Script>
     <body
       className={`${bandung.className} font-bandung antialiased bg-gray-900 text-gray-200 tracking-tight`}>
     <div className="flex flex-col min-h-screen overflow-hidden">
